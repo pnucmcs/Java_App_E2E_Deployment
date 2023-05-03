8d14edd809f4429c12389dbb5780df6aac995560
Prashanth Narahari <prashanthnarahari@Prashanths-MacBook-Pro.local>
2023-05-03 16:09:19 -0400

    from vscode

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -8,6 +8,6 @@ public class MyController {
     
     @GetMapping("/test-docker")
     public String getData() {
-        return "in docker tutorial automatic pipeline v3";
+        return "in docker tutorial automatic pipeline v4";
     }
 }
